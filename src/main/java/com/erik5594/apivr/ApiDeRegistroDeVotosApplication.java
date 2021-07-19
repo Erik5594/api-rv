2183d92f7b31e135ab388fed06ca80349b821491
erik-qo <erik.derick74@gmail.com>
2021-07-19 19:09:51 -0300

    Criando o projeto API-RV

---
+++ src/main/java/com/erik5594/apivr/ApiDeRegistroDeVotosApplication.java
@@ -0,0 +1,13 @@
+package com.erik5594.apivr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeRegistroDeVotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeRegistroDeVotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erik5594/apivr/ApiDeRegistroDeVotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erik5594.apivr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeRegistroDeVotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
