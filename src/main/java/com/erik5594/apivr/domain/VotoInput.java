d4f7af7849228b3b7b4a03626fb17ac6753221a6
erik-qo <erik.derick74@gmail.com>
2021-07-20 14:26:34 -0300

    Adcionando documentação na API

---
+++ src/main/java/com/erik5594/apivr/domain/VotoInput.java
@@ -0,0 +1,8 @@
+package com.erik5594.apivr.domain;
+
+/**
+ * @author erik_
+ * Data Criacao: 20/07/2021 - 14:05
+ */
+public class VotoInput {
+}
---
+++ src/main/java/com/erik5594/apivr/openapi/SpringFoxConfig.java
@@ -0,0 +1,8 @@
+package com.erik5594.apivr.openapi;
+
+/**
+ * @author erik_
+ * Data Criacao: 20/07/2021 - 10:59
+ */
+public class SpringFoxConfig {
+}
